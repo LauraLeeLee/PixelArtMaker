51a0bc0d4ebf4c5d5d9acabf8a14b4ae238f7b18
Laura Logan <laurardh@aol.com>
2018-02-23 13:44:29 -0500

    adds event listeners to allow continual painting when mouse is held down. (not working)

--- designs.js
+++ designs.js
@@ -20,17 +20,52 @@ function makeGrid(h, w) {
     }
   }
 }
+//boolean to check if in drag mode (mousedown and hold)
+let isDrag = false;
 
-//adds color to clicked cell on grid
-canvas.addEventListener('click', function(event){
+//checks mouse button is released
+canvas.addEventListener('mouseup', function(event){
+  isDrag = false;
+});
 
+let paintCanvas = function(){
   // Select color input
   const colorPicked = document.querySelector('#colorPicker').value;
+  isDrag = false;
   //checks to see if on the event of 'click' a TD was clicked on
   if (event.target.nodeName === 'TD'){
-    //event.target will target the specific TD that is clicked 
+    //event.target will target the specific TD that is clicked
     event.target.style.backgroundColor = colorPicked;
     console.log("am I coloring?");
     console.log('This was clicked  ' + event.target);
+  }
+}
+
+//adds color to one clicked cell on grid
+canvas.addEventListener('click', function(event){
+  paintCanvas();
+});
+
+//eventListener to add mousedown -step one for continuous painting instead of per click
+canvas.addEventListener('mousedown', function(event){
+  //shuts of its default as we aren't actually going to drag and drop
+  event.preventDefault();
+  //our boolean to check if mouse is clicked and still held down
+  isDrag = true;
+});
+
+//eventListener to add mouseover to 'paint' while mouse is held down and moving
+canvas.addEventListener('mouseover', function(event){
+  //checks if mouse button is still pressed down while passing over canvas
+  if(isDrag === true) {
+    paintCanvas();
+    console.log('continuous painting');
+  }
+});
+
+//adds doubleclick to 'erase' color from one cell
+canvas.addEventListener('dblclick', function(event){
+  if(event.target.nodeName === 'TD'){
+    event.target.style.backgroundColor = '#fff';
   }
 });
